e57e06f685b3d4a56f5364c2815f4935a19bc695
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-05-16 21:06:15 -0400

    feat(nvim): use new v0.10.0 `default` colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -2,13 +2,14 @@ local notify = require("blaz.helper.notify")
 
 -- Wait until after all the plugins load to set the colorscheme
 -- vim.cmd([[autocmd vimenter * ++nested colorscheme gruvbox]])
-vim.cmd.colorscheme("default")
+-- vim.cmd.colorscheme("default")
 
-local colorscheme = "gruber-darker"
+local colorscheme = "default"
+-- local colorscheme = "gruber-darker"
 -- local colorscheme = "paper"
 
-local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
-if not status_ok then
+local ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
+if not ok then
 	notify.warn("Colorscheme", colorscheme .. " colorscheme not found!")
 	return
 end
