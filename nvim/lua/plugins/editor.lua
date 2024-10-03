16de5767223cc6e9b7b38a704bc8356dce72d0f3
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-10-02 22:05:50 -0400

    feat(nvim): update plugins and install `vim-easy-align`

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,4 +14,11 @@ return {
 	"kyazdani42/nvim-web-devicons", -- for file icons
 	{ "numToStr/Comment.nvim", config = true },
 	"tpope/vim-sleuth",
+	{
+		"junegunn/vim-easy-align",
+		config = function()
+			vim.keymap.set("x", "ga", "<Plug>(EasyAlign)")
+			vim.keymap.set("n", "ga", "<Plug>(EasyAlign)")
+		end,
+	},
 }
